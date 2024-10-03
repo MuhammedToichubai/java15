a7f16cb5773cd1083626e19c38efee00896c10fd
Mukhammed Dev <muhammedtoichubai@gmail.com>
2024-10-03 19:23:38 +0600

    added text in Hibernate class

--- src/main/java/mukhammed/Hibernate.java
+++ src/main/java/mukhammed/Hibernate.java
@@ -1,4 +1,47 @@
 package mukhammed;
 
 public class Hibernate {
+    public static void main(String[] args) {
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+        System.out.println("Java 15 is the best");
+    }
 }
